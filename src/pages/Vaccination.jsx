9969f5ac6d0c6c864527ce2a35f3626e3df86d7e
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2021-10-31 23:49:01 -0300

    Add Vaccination and UserManagement pages

--- src/pages/Vaccination.jsx
+++ src/pages/Vaccination.jsx
@@ -5,58 +5,95 @@ import Form from "../components/Form";
 import Column from "../components/Column";
 import Button from "../components/Button";
 import Select from "../components/Select";
+import ErrorHandler from "../utils/errorHandler";
+import {AxiosResponse} from "axios";
+import vaccination from "../utils/requests/vaccination";
+import vaccine from "../utils/requests/vaccine";
 
 class Vaccination extends React.Component {
-  defaultProps = {
-    // title: "",
-    className: "",
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      vaccines: [],
+      vaccination: [],
+      selectedVaccine: {},
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const vaccineResponse = await vaccine.getAll();
+      const vaccinationResponse = await vaccination.getAll();
+      this.setState({
+        vaccines: vaccineResponse.data.vaccines,
+        vaccination: vaccinationResponse.data.vaccination,
+      });
+    } catch(e) {
+      ErrorHandler.handleRequestError(e);
+    }
+  }
+
+  async registerVaccination() {
+    try {
+      const vaccineId = this.state.selectedVaccine;
+      const lot = document.getElementById("lot").value;
+      const date = document.getElementById("application-date").value;
+      const response: AxiosResponse = await vaccination.create({ lot, date, vaccineId });
+      this.setState({
+        vaccination: [response.data.vaccination, ...this.state.vaccination],
+      });
+    } catch (e) {
+      ErrorHandler.handleRequestError(e);
+    }
+  }
 
   render() {
-    // const props = {...this.defaultProps, ...this.props};
-    return (
-        <>
-        <Row className="">
-          <Column span="twelve">
-            <Form title="Vaccination">
-              <Row>
-                <Select span="five" items={{OK: "OKa", NOK: "NOKa"}}/>
-                <Input span="four" type="date" placeholder="11/22/225"/>
-                <Button span="three">Register</Button>
-              </Row>
-              {/*<Row>*/}
+    const {vaccines, vaccination, selectedVaccine} = this.state;
 
-              {/*</Row>*/}
+    return (
+      <Row className="content-container">
+        <Column span="offset-by-one ten">
+          <Row>
+            <Form title="Vaccination" className="title">
+              <Row>
+                <Select span="five" label="VACCINE" value={selectedVaccine} items={
+                  vaccines.reduce((options, vaccine) => {
+                    // options[vaccine.id] = `${vaccine.name} - ${vaccine.manufacturer}`;
+                    options[vaccine.id] = vaccine.name;
+                    return options;
+                  }, {})} onChange={(event) => this.setState({ selectedVaccine: event.target.value })}/>
+                <Input span="three" label="LOT" placeholder="XXNNXNNNXXX-N"/>
+                <Input span="four" label="APPLICATION DATE" type="date"/>
+              </Row>
+              <Row>
+                <Button onClick={() => this.registerVaccination()}>Register</Button>
+              </Row>
             </Form>
-          </Column>
-        </Row>
-    <Row className="centered">
-      <table className="u-full-width">
-        <thead>
-        <tr>
-          <th>Name</th>
-          <th>Age</th>
-          <th>Sex</th>
-          <th>Location</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Dave Gamache</td>
-          <td>26</td>
-          <td>Male</td>
-          <td>San Francisco</td>
-        </tr>
-        <tr>
-          <td>Dwayne Johnson</td>
-          <td>42</td>
-          <td>Male</td>
-          <td>Hayward</td>
-        </tr>
-        </tbody>
-      </table>
-    </Row>
-        </>
+          </Row>
+          <Row>
+            {vaccination.length !== 0 ?
+              <table className="u-full-width">
+                <tbody>
+                  {vaccination.map((vaccinationRecord, index) => {
+                    const vaccine = vaccines.find(vaccine => vaccine.id === vaccinationRecord.vaccineId);
+                    return (
+                      <tr key={index}>
+                        <td className="five w-columns">{vaccine.name}</td>
+                        <td className="four w-columns">{vaccinationRecord.lot}</td>
+                        <td className="three w-columns">{vaccinationRecord.date}</td>
+                        {/*<td className="one w-columns">*/}
+                        {/*  <Input type="button" value="Edit"/>*/}
+                        {/*</td>*/}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+              : ""
+            }
+          </Row>
+        </Column>
+      </Row>
     );
   }
 }
--- src/pages/admin/UserManagement.jsx
+++ src/pages/admin/UserManagement.jsx
@@ -1,59 +1,126 @@
 import * as React from "react";
 import Row from "../../components/Row";
-import Button from "../../components/Button";
-import Input from "../../components/Input";
-import Select from "../../components/Select";
-import Form from "../../components/Form";
 import Column from "../../components/Column";
+import ErrorHandler from "../../utils/errorHandler";
+import usersRequest from "../../utils/requests/users";
+import Select from "../../components/Select";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import {copyObjectsArray} from "../../utils/data";
 
 class UserManagement extends React.Component {
-  defaultProps = {
-    // title: "",
-    className: "",
-  };
+  constructor(props) {
+    super(props);
+    this.unchangedUsers = [];
+    this.state = {
+      users: [],
+      editable: false,
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await usersRequest.getAll();
+      this.setState({
+        users: response.data.users,
+      });
+      // Copy objects by value
+      this.unchangedUsers = copyObjectsArray(this.state.users);
+    } catch(e) {
+      ErrorHandler.handleRequestError(e);
+    }
+  }
+
+  async save() {
+    await usersRequest.update(this.state.users);
+    this.unchangedUsers = copyObjectsArray(this.state.users);
+    this.toggleEditing();
+  }
+
+  cancelEditing() {
+    this.setState({
+      users: copyObjectsArray(this.unchangedUsers),
+    });
+    this.toggleEditing();
+  }
+
+  toggleEditing() {
+    this.setState({ editable: !this.state.editable });
+  }
+
+  isEditable() {
+    return this.state.editable;
+  }
+
+  onUserTypeChange(event, row) {
+    const usersTmp = [...this.state.users];
+    usersTmp[row].type = event.target.value;
+
+    this.setState({ users: usersTmp });
+  }
 
   render() {
-    // const props = {...this.defaultProps, ...this.props};
+    const {users, editable} = this.state;
+
     return (
-        <>
-          <Row className="">
-            <Column span="twelve">
-              <Form title="Vaccines">
+      <Row className="content-container">
+        <Column span="offset-by-two eight">
+          <Row>
+            <Column span="twelve"><h4 className="title centered">Users</h4></Column>
+          </Row>
+          {users.length !== 0 ?
+            <Row>
+              {editable ?
                 <Row>
-                  <Input span="five" placeholder="Name"/>
-                  <Input span="four" placeholder="Manufacturer"/>
-                  <Button span="three">Create</Button>
+                  <Input span="six" type="button" value="Cancel" className=""
+                         onClick={() => this.cancelEditing()}/>
+                  { editable &&
+                    <Button span="six" style={{float: "right"}} className=""
+                            onClick={() => this.save()}>Save</Button> }
                 </Row>
-              </Form>
-            </Column>
-          </Row>
-          <Row className="centered">
-            <table className="u-full-width">
-              <thead>
-              <tr>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Sex</th>
-                <th>Location</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td>Dave Gamache</td>
-                <td>26</td>
-                <td>Male</td>
-                <td>San Francisco</td>
-              </tr>
-              <tr>
-                <td>Dwayne Johnson</td>
-                <td>42</td>
-                <td>Male</td>
-                <td>Hayward</td>
-              </tr>
-              </tbody>
-            </table>
-          </Row>
-        </>
+                :
+                <Row>
+                  <Input style={{float: "right"}} type="button" span="twelve" value="Edit" className=""
+                         onClick={() => this.toggleEditing()}/>
+                </Row>
+              }
+              <table className="u-full-width">
+                <thead>
+                  <tr className="info-title">
+                    <th>Email</th>
+                    <th>Type</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {users.map((user, row) => {
+                  return (
+                    this.isEditable()?
+                    <tr key={row}>
+                      <td className="six w-columns">{user.email}</td>
+                      <td className="five w-columns">
+                        <Select value={user.type} items={usersRequest.USER_TYPE_ENUM}
+                                onChange={(event) => this.onUserTypeChange(event, row)}/>
+                      </td>
+                    </tr>
+                    :
+                    <tr key={row}>
+                      <td className="five w-columns">{user.email}</td>
+                      <td className="four w-columns" style={{textTransform: "uppercase"}} >
+                        {user.type}
+                      </td>
+                      {/*<td className="one w-columns">*/}
+                      {/*  <Input type="button" value="Edit" onClick={() => this.toggleEditable(row)}/>*/}
+                      {/*</td>*/}
+                    </tr>
+                  );
+                })}
+                </tbody>
+              </table>
+            </Row>
+            : ""
+          }
+        </Column>
+      </Row>
     );
   }
 }
